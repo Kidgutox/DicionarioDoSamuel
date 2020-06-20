ff5e36051ecf9c477ca3267366b9f1652b631d48
Gustavo <gustavocsantosalmeida80@gmail.com>
2020-06-20 01:59:09 -0300

    Projeto finalizado

---
+++ src/Testes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Elias
+ */
+public class Testes {
+    public static void main (String args []){
+        String letra1 = "Felipe";
+        String letra2 = "Fernando";
+        String letra3 = "Fernanda";
+
+System.out.println(letra1.compareTo(letra2));
+System.out.println(letra2.compareTo(letra3));
+System.out.println(letra2.compareTo(letra2));
+
+    }
+}
--- src/dicionario.java
+++ src/dicionario.java
@@ -1,4 +1,25 @@
+/*
+Entrega do Trabalho 2- Algoritmos e Programação II
+Nós,
+Gustavo Costa Santos Almeida
+Luís Felipe da Costa Angelo
+declaramos que
+todas as respostas são fruto de nosso próprio trabalho,
+não copiamos respostas de colegas externos à equipe,
+não disponibilizamos nossas respostas para colegas externos ao grupo e
+não realizamos quaisquer outras atividades desonestas para nos beneficiar ou
+prejudicar outros.
 
+ /* Dados importantes
+
+Data de entrega: 22/06/2020
+Curso: Bacharelado em Jogos Digitais
+Semestre: 3°
+Matéria: Algoritmo e programação II
+
+ */
+
+//Importações necessárias
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,16 +29,21 @@ public class dicionario {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
-        FileReader le = new FileReader("entrada2.txt");
+        FileReader le = new FileReader("entrada.txt");
 
-        // bufferiza a entrada do arquivo para ser possivel ler uma linha inteira no arquivo
+        // bufferiza a entrada do arquivo para ser possivel ler uma linha inteira no
+        // arquivo
         BufferedReader leBufferizado = new BufferedReader(le);
 
+        // lê uma linha do arquivo de entrada
         String texto = "";
         String parada = ".";
-        int numeroDePalavras = 0;
-        String[] vetorComTodasAsPalavras = new String[1000];
+        String[] vetorComTodasAsPalavras = {};
 
+        /*
+	O trecho abaixo é responsável por realizar a leitura de todas as linhas do
+	arquivo e por diminuir todas as letras do arquivo que estiverem maiusculas
+         */
         while (!texto.equals(parada)) {
             // aqui lido uma linha do arquivo
             texto = leBufferizado.readLine();
@@ -25,61 +51,81 @@ public class dicionario {
             texto = texto.toLowerCase();
             // gera um vetor com as palavas da linha
             String palavras[] = texto.split(" ");
-
-            // insere as palavras no dicionario do samuel ( vetorComTodasAsPalavras)
-            // para cada uma das palavras em uma linha
             for (int i = 0; i < palavras.length; i++) {
-                // faz a busca binaria da palavra no vetor com todas as palavras
-                if (!palavras[i].equals("") && !palavras[i].equals(".") && buscabinaria(vetorComTodasAsPalavras, palavras[i]) != -1) {
-                    // se nao achar a palavra no vetorComTodasAsPalavras, entao
-                    // insere a palavra
-                    InserePalavra(vetorComTodasAsPalavras, palavras[i], numeroDePalavras);
-                    // aumenta o numero de palavras no vetor
-                    numeroDePalavras++;
+                if (!palavras[i].equals("") && !palavras[i].equals(".") && (buscaBinaria(vetorComTodasAsPalavras, palavras[i], 0, vetorComTodasAsPalavras.length) == false)) {
+                    vetorComTodasAsPalavras = totalDePalavras(vetorComTodasAsPalavras, palavras[i], vetorComTodasAsPalavras.length + 1);
+                    vetorComTodasAsPalavras = inserePalavra(vetorComTodasAsPalavras);
                 }
             }
         }
-        // impreme as palavras inseridas no vetor, somente as inseridas
-        for (int i = 0; i < numeroDePalavras; i++) {
-            System.out.println(vetorComTodasAsPalavras[i]);
-        }
+        imprimeVetor(vetorComTodasAsPalavras);
     }
 
-    public static void InserePalavra(String[] vetorAtual, String novaPalavra, int qtdvetor) {
-        // estou inserindo no final do vetor
-        vetorAtual[qtdvetor] = novaPalavra;
-        int i = qtdvetor; // comeca no fina do vetor (qtdvetor)
-        while (i > 0 && vetorAtual[i].compareTo(vetorAtual[qtdvetor]) < 0) { //palavras[i].compareTo(palavras[j]) < 0
-            vetorAtual[i] = vetorAtual[qtdvetor];
-            i--;
-        }
-        vetorAtual[i] = novaPalavra;
+    /*
+    A função abaixo é responsável por criar um vetor com N posições
+    (N = quantidade de palavras do txt).
+     */
+    public static String[] totalDePalavras(String[] vetorAtual, String novaPalavra, int tamanhoDoVetorASerCriado) {
+        String[] novoVetor = new String[tamanhoDoVetorASerCriado];
 
-        /* int i=j; // comeca no fina do vetor (qtdvetor)
-         int x = v[i]; // int x = v[i]
-         while(i > 0 && x < v[i-1] ){ //palavras[i].compareTo(palavras[j]) < 0
-            v[i] = v[i-1];
-            i--;
-         }
-         v[i]=x;
-         */
-    }
-
-    public static int buscabinaria(String palavras[], String x) {
-        int i, m, f;
-        i = 0;
-        f = palavras.length - 1;
-        while (i <= f) {
-            m = (i + f) / 2;
-            if (palavras[m].equals(x)) { //meio
-                return m;
-            }
-            if (x.compareTo(palavras[m]) < 0){ // esquerda 
-                f = m - 1;
-            }else{ // x > palavras[m] - direita
-                i = m + 1;
+        for (int i = 0; i < novoVetor.length; i++) {
+            if (i < vetorAtual.length) {
+                novoVetor[i] = vetorAtual[i];
+            } else {
+                novoVetor[i] = novaPalavra;
             }
         }
-        return -1;
+        return novoVetor;
     }
+
+    /*
+    A função abaixo realiza a inserção e a troca de posições
+    das palavras do dicionário
+     */
+    public static String[] inserePalavra(String[] vetorAtual) {
+        String tempPalavra;
+        int j = vetorAtual.length - 1;
+
+        // Realiza a comparação e troca de posições
+        for (int i = j; i > 0; i--) {
+            if (vetorAtual[i].compareTo(vetorAtual[i - 1]) < 0) {
+                tempPalavra = vetorAtual[i - 1];
+                vetorAtual[i - 1] = vetorAtual[i];
+                vetorAtual[i] = tempPalavra;
+            }
+        }
+
+        return vetorAtual;
+    }
+
+    /*
+    Função responsável por verificar se a palavra já existe no dicionário
+     */
+    public static boolean buscaBinaria(String vetorComTodasAsPalavras[], String palavraAtual, int inicio, int fim) {
+        int meio;
+        // realiza o cálculo absoluto do meio do vetor
+        meio = Math.abs((inicio + fim) / 2);
+        if (vetorComTodasAsPalavras.length != meio) {
+            if (palavraAtual.compareTo(vetorComTodasAsPalavras[meio]) == 0) { // meio do vetor
+                return true; // Retorna true pois, encontrou a palavra no dicionário
+            } else if (fim > inicio) {
+                if (palavraAtual.compareTo(vetorComTodasAsPalavras[meio]) < 0) { // direita do vetor
+                    return buscaBinaria(vetorComTodasAsPalavras, palavraAtual, inicio, meio - 1);
+
+                } else if (palavraAtual.compareTo(vetorComTodasAsPalavras[meio]) > 0) { // esquerda do vetor
+                    return buscaBinaria(vetorComTodasAsPalavras, palavraAtual, meio + 1, fim);
+                }
+            }
+        }
+        return false; // Retorna false pois, a palavra não se enquadra nas condições acima
+    }
+
+    //Imprime o vetor
+    public static void imprimeVetor(String[] vetorAtual) {
+        for (int i = 0; i < vetorAtual.length; i++) {
+            System.out.println(vetorAtual[i]);
+        }
+        System.out.println("A quantidade total de palavras no dicionário são: " + vetorAtual.length);
+    }
+
 }
